aec3525cd6eab11286cb5fcba5848b36a4513d35
Mehboob15 <mehbooboad15@gmail.com>
2024-10-04 23:50:56 -0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,11 @@ const Page = () => {
       <p>Hello world</p>
       <p>my name is khan</p>
       <p>hello worlduser</p>
+<div>
+  <h1>my father</h1>
+  <p>Mehboob</p>
 
+</div>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvHNqjDoE_DKiwwCRShMS83zAJaPTMmGwufg&s" alt="" />
     </div>
   )
